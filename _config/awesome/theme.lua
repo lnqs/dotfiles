7b89441737f4d96f835e6d28ba845071ae07f190
Lynx <simon.schoenfeld@gmail.com>
2015-10-06 00:23:31 +0200

    Use symlink to find wallpaper

--- _config/awesome/theme.lua
+++ _config/awesome/theme.lua
@@ -2,7 +2,7 @@ themepath = "/usr/share/awesome/themes/zenburn/theme.lua"
 
 theme = dofile(themepath)
 
-theme.wallpaper         = home .. "/.wallpapers/51882.jpg"
+theme.wallpaper         = home .. "/.wallpaper"
 theme.font              = "sans 7"
 theme.border_width      = 1
 
