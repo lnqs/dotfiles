00d3cc70a38d3065d09d41a5de8e2089215dc758
Simon Hayessen <simon@lnqs.io>
2023-06-12 12:44:22 +0200

    Use NvChad for nvim config

---
+++ _config/nvim/lua/custom/init.lua
@@ -0,0 +1,9 @@
+local opt = vim.opt
+
+-------------------------------------- options ------------------------------------------
+opt.backup = false
+opt.writebackup = false
+opt.swapfile = false
+opt.list = true
+opt.listchars = { tab = "» ", trail = "·" }
+
