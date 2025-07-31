736d6563159de4a7d54aafdff369c53a2d9c2a89
Simon Hayessen <simon@lnqs.io>
2025-07-31 21:21:48 +0200

    Allow unknown at rules in cssls in nvim

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -2,6 +2,16 @@ require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls", "pyright", "bashls", "ts_ls", "clangd" }
 
+vim.lsp.config('cssls', {
+  settings = {
+    css = {
+      lint = {
+        unknownAtRules = 'ignore',
+      },
+    },
+  },
+})
+
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
